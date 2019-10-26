e9e23040d40eb765b64811b80c36d50296f3ca5e
witt3rd <witt3rd@witt3rd.com>
2019-10-26 09:32:40 -0700

    initial commit

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Trigger : MonoBehaviour
+{
+    public PlayableDirector director;
+
+    private void OnMouseDown()
+    {
+        director.Play();
+    }
+}
